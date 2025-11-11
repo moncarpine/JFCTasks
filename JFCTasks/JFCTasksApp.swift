ef9d531a167d6b7068da42bfc89ab6735715cb70
Monty Carlo Pineda <moncarpinew@gmail.com>
2025-11-12 03:40:07 +0800

    Initial Commit

---
+++ JFCTasks/JFCTasksApp.swift
@@ -0,0 +1,20 @@
+//
+//  JFCTasksApp.swift
+//  JFCTasks
+//
+//  Created by Monty Carlo Pineda on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct JFCTasksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
